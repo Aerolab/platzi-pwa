5f1385c7deabfc02f7a7d7198f83c3efcc9ab935
Roberto Gonzalez <roberto@aerolab.co>
2018-07-09 17:33:28 -0300

    4.WebShare

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -21,6 +21,24 @@ export default class Recipe extends React.Component {
     this.setState({ recipe, isLoading: false })
   }
 
+  compartir = (e) => {
+    e.preventDefault()
+    if( ! navigator.share ) {
+      alert("Tu browser no soporta la Web Share API"); 
+      return;
+    }
+
+    const { recipe } = this.state
+
+    navigator.share({
+      title: `${recipe.name}`,
+      text: 'Receta de Platzi',
+      url: document.location.href
+    })
+      .then(() => alert('Contenido compartido!'))
+      .catch((error) => alert('Hubo un error'))
+  }
+
   render() {
     const { recipe, isLoading } = this.state
 
@@ -44,6 +62,7 @@ export default class Recipe extends React.Component {
           <p>{ recipe.origin }</p>
         </div>
         <div>
+          <a className="share" onClick={ this.compartir }>Compartir</a>
         </div>
       </div>
 
