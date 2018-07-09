bfd52afa5251d4123010d1e84d0d1bcc1949abcc
Roberto Gonzalez <roberto@aerolab.co>
2018-07-09 17:29:06 -0300

    3.Analytics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from "react-router-dom"
+import { Router, Route, Link } from "react-router-dom"
 import Home from './pages/Home'
 import Recipe from './pages/Recipe'
 import Timer from './pages/Timer'
 import './App.css'
 
+import { createBrowserHistory } from 'history'
+import ReactGA from 'react-ga'
+
+const history = createBrowserHistory()
+
+ReactGA.initialize('UA-000000-01')
+ReactGA.pageview(window.location.pathname + window.location.search)
+
+history.listen(function(location) {
+  ReactGA.pageview(window.location.pathname + window.location.search)
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router history={ history }>
         <div>
           <header>
             <Link to="/">Recetas</Link>
@@ -21,7 +32,7 @@ export default class App extends React.Component {
             <Route path="/timer" component={Timer} />
           </main>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
